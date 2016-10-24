c807e12a267ec7e4d949e6f7f863e64bbde00520
Mali Michael <malimccalla@gmail.com>
2016-10-24 13:52:19 +0100

    Adds confirmation and error message

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,15 @@ $(document).ready(function() {
     var $email = $('input').val();
     event.preventDefault();
     if ( /(.+)@(.+){2,}\.(.+){2,}/.test($email) ) {
-      $.post('http://flare-app-server.appspot.com/', { email: $email })
+      $.post('http://flare-app-server.appspot.com/', { email: $email });
       $('input').val('');
+      $('.message').removeClass('red');
+      $('.message').addClass('green');
+      $('.message').text('Thanks! Please check your email for your invite.');
+    } else {
+      $('.message').removeClass('green');
+      $('.message').addClass('red');
+      $('.message').text('Please enter a valid email.');
     }
-    // console.log($email);
   })
 });
